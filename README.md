456281bf054b1c5a8b0b71e47095143721b08d58
Bicepsfako <bicepsfako@gmail.com>
2021-04-11 14:29:46 +0300

    v1.0.0

---
+++ README.md
@@ -0,0 +1,38 @@
+# Plugin Manager for Discord Bots.
+---
+A module that manages plugins that will easily add new features to your Discord Bot.
+
+[Discord](https://discord.gg/cW2ntzQ3fb).
+---
+
+## Installation
+
+```shell
+npm install plugincord
+```
+
+### Launch of the module
+
+```javascript
+const PluginManager = require('plugincord');
+
+const manager = new PluginManager(client);
+
+manager.start("./src/plugins"); // Full path of directory that includes plugin files.
+```
+
+### Plugin Example
+
+```js
+const fs = require('fs');
+
+module.exports = {
+    name: "example-plugin",
+	run(client) {
+        const channel = client.channels.cache.get("0000000000000000");
+        setInterval(() => {
+            channel.send("This message sended via " + this.name + " plugin!");
+        }, 60 * 1000)
+	},
+};
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+module.exports = class PluginManager {
+    constructor(client) {
+        if (!client) return Throw("[Plugin Manager] You need to send client object to class.")
+        this.client = client;
+    }
+
+    start(dir) {
+        if (!dir) throw new Error("[Plugin Manager] You need to specify plugins directory.");
+
+        try {
+            const plugins = fs.readdirSync(dir).filter(file => file.endsWith('.plugin.js'));
+            if (plugins.length === 0) return Throw("[Plugin Manager] There is no plugin files, be sure that plugin names ends with .plugin.js");
+            console.log(`[Plugin Manager] Starting ${plugins.length} plugins...`);
+            for (const file of plugins) {
+                const plugin = require(`./plugins/${file}`);
+                plugin.run(this.client);
+                delete require.cache[file];
+            }
+            console.log(`[Plugin Manager] Plugins has been started.`);
+        } catch (error) {
+            throw new Error("[Plugin Manager] " + error)
+        }
+    }
+}
